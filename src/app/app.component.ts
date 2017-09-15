4bc710b9a666c15522ed34e11fccbeaaff11c2d9
Mike Dacanay <migd64@gmail.com>
2017-09-14 19:20:48 -0700

    added pipes/lowercase filter to email, interpolation on importance is set to boolean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Dojo Mail';
 	emails = [
-		{'email':"bill@gates.com", 'Importance':'High', 'Subject':'Python', 'Content':'aodsfjapsjdof'},
-		{'email':"ping@gmail.com", 'Importance':'Medium', 'Subject':'Javscript', 'Content':'asdfasdfasdwhatever'},
-		{'email':"pong@gmail.com", 'Importance':'Okish', 'Subject':'Kobal', 'Content':'4joti4843535'}
+		{'email':"BILL@gates.com", 'Importance':true, 'Subject':'Python', 'Content':'aodsfjapsjdof'},
+		{'email':"ping@gmail.com", 'Importance':false, 'Subject':'Javscript', 'Content':'asdfasdfasdwhatever'},
+		{'email':"pong@gmail.com", 'Importance': true, 'Subject':'Kobal', 'Content':'4joti4843535'},
 	];
 }
